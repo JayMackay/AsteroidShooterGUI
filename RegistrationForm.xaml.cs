8da633ae0da282dd4ac1e0daa78ed748cb3847d9
JayMackay <jared.mackay1994@live.com>
2020-03-04 21:39:16 +0000

    User Registration Initialization & Password Requirement Check

--- RegistrationForm.xaml.cs
+++ RegistrationForm.xaml.cs
@@ -10,11 +10,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Navigation;
+using AsteroidShooterGUI.Database_Models;
+using Microsoft.Data.SqlClient;
 
 namespace AsteroidShooterGUI
 {
     public partial class RegistrationForm : Window
     {
+
+        private User newUser;
+
+        //Initialize Registration Form
         public RegistrationForm()
         {
             InitializeComponent();
@@ -27,7 +33,79 @@ namespace AsteroidShooterGUI
             {
                 DragMove();
             }
+        }
 
+        //RESET TEXT BOX METHOD
+        public void Reset()
+        {
+            firstNameText.Text = "";
+            lastNameText.Text = "";
+            usernameText.Text = "";
+            passwordText.Password = "";
+            passwordConfirmText.Password = "";
+        }
+
+        //BACK REDIRECT METHOD
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow MainWindowObject = new MainWindow();
+            MainWindowObject.Show();
+            this.Close();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            //User Textbox Input Fields
+            string firstName = firstNameText.Text;
+            string lastName = lastNameText.Text;
+            string userName = usernameText.Text;
+            string password = passwordText.Password;
+
+            //Password Parameters
+            if (passwordText.Password.Length == 0)
+            {
+                errormessage.Text = "Please enter a password";
+                passwordText.Focus();
+            }
+
+            //Password Confirmation
+            else if (passwordConfirmText.Password.Length == 0)
+            {
+                errormessage.Text = "Please confirm your password";
+                passwordConfirmText.Focus();
+            }
+            else if (passwordText.Password != passwordConfirmText.Password)
+            {
+                errormessage.Text = "Password is not matching";
+                passwordConfirmText.Focus();
+            }
+
+            //SQL Database Entry
+            else
+            {
+                errormessage.Text = "";
+
+                //Set SQL Credentials
+                SqlConnection connection = new SqlConnection(
+                    "Data Source = localhost;" +
+                    "Initial Catalog = UserDatabase;" +
+                    "User ID = SA;" +
+                    "Password= Passw0rd2018;");
+
+                //Open New SQL Connection
+                connection.Open();
+
+                //SQL DML Query
+                SqlCommand command = new SqlCommand(
+                    "INSERT INTO Users " +
+                    "(firstName, lastName, userName, password) " +
+                    "VALUES('" + firstName + "','" + lastName + "','" + userName + "','" + password + "')", connection);
+
+                command.ExecuteNonQuery();
+                errormessage.Text = "You have registered successfully!";
+                Reset();
+            }
         }
     }
 }
+
